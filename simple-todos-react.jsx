47cd6c83142350af7e46ed319cd6d23bccdcff08
alexnicolini <alexnicolini@gmail.com>
2015-11-05 22:12:20 -0200

    Added methods for add, remove, update task

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -16,3 +16,27 @@ if (Meteor.isClient) {
     React.render(<App />, document.getElementById('render-target'));
   });
 }
+
+Meteor.methods({
+  addTask(text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.error("not-authorized"); // Launch an exception
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),            // current time
+      owner: Meteor.userId(),           // _id of logged in user 
+      username: Meteor.user().username  // username of logged in user
+    });
+  },
+
+  removeTask(taskId) {
+    Tasks.remove(taskId);
+  },
+
+  setChecked(taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  }
+});
